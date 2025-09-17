b02f6afb9e0556bf9411be22ca9308a3519cba76
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-09-17 18:33:33 +0200

    feat_ update application properties

--- src/test/java/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users (username, name, email, password, role)
-VALUES ('TestUser', 'nameTest', 'usertnest@test.com', 'password123', 'USER');
-
-INSERT INTO users (username, name, email, password, role)
-VALUES ('TestUser2', 'nameTest2', 'user2tnest@test.com', 'password123', 'ADMIN');
-
-
-
