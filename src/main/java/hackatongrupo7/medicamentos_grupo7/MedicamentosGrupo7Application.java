71630c7d9dbb65076132fcb66dc20312abab5f34
Efren TC <efrentomascampa@gmail.com>
2025-09-17 10:40:02 +0200

    Initial commit

---
+++ src/main/java/hackatongrupo7/medicamentos_grupo7/MedicamentosGrupo7Application.java
@@ -0,0 +1,13 @@
+package hackatongrupo7.medicamentos_grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicamentosGrupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicamentosGrupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/hackatongrupo7/medicamentos_grupo7/MedicamentosGrupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package hackatongrupo7.medicamentos_grupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicamentosGrupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
