0b22a4944fa3433772cc09dc78c292ea4d504354
Dmytro Belei <beleyhha@gmail.com>
2025-09-18 13:56:16 +0200

    refactor: add unique= true into alergyEntity

--- src/main/java/hackatongrupo7/medicamentos_grupo7/allergy/Allergy.java
+++ src/main/java/hackatongrupo7/medicamentos_grupo7/allergy/Allergy.java
@@ -15,7 +15,7 @@ public class Allergy {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(unique = true, nullable = false, length = 100)
     private String name;
 
 }
