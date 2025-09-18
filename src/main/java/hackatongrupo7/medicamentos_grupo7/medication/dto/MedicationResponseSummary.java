8ff3007a4df5a271644ed96b008c15131c038699
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-09-18 21:31:53 +0200

    feat:add id on medication summary

--- src/main/java/hackatongrupo7/medicamentos_grupo7/medication/dto/MedicationResponseSummary.java
+++ src/main/java/hackatongrupo7/medicamentos_grupo7/medication/dto/MedicationResponseSummary.java
@@ -1,6 +1,7 @@
 package hackatongrupo7.medicamentos_grupo7.medication.dto;
 
 public record MedicationResponseSummary(
+        Long id,
         String name,
         int dose,
         boolean taken) {
